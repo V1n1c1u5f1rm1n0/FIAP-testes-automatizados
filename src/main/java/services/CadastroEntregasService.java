c080a210a885f37b7c375eb70421717cf71e931a
Vinicius Firmino <you@example.com>
2024-11-01 15:32:06 -0300

    first commit

---
+++ src/main/java/services/CadastroEntregasService.java
@@ -0,0 +1,61 @@
+package services;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import model.EntregaModel;
+
+import static io.restassured.RestAssured.given;
+public class CadastroEntregasService<JSONObject, JSONTokener> {
+
+
+    final EntregaModel entregaModel = new EntregaModel();
+    public final Gson gson = new GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
+    public Response response;
+    String baseUrl = "http://localhost:8080";
+    public String idDelivery;
+    String schemasPath = "src/test/resources/schemas/";
+    JSONObject jsonSchema;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public void setFieldsDelivery(String field, String value) {
+        switch (field) {
+            case "numeroPedido" -> entregaModel.setNumeroPedido(Integer.parseInt(value));
+            case "nomeEntregador" -> entregaModel.setNomeEntregador(value);
+            case "statusEntrega" -> entregaModel.setStatusEntrega(value);
+            case "dataEntrega" -> entregaModel.setDataEntrega(value);
+            default -> throw new IllegalStateException("Unexpected feld" + field);
+        }
+    }
+    public void createDelivery(String endPoint) {
+        String url = baseUrl + endPoint;
+        String bodyToSend = gson.toJson(entregaModel);
+        response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(bodyToSend)
+                .when()
+                .post(url)
+                .then()
+                .extract()
+                .response();
+    }
+    public void retrieveIdDelivery() {
+        idDelivery = String.valueOf(gson.fromJson(response.jsonPath().prettify(), EntregaModel.class).getNumeroEntrega());
+    }
+    public void deleteDelivery(String endPoint) {
+        String url = String.format("%s%s/%s", baseUrl, endPoint, idDelivery);
+        response = given()
+                .accept(ContentType.JSON)
+                .when()
+                .delete(url)
+                .then()
+                .extract()
+                .response();
+    }
+
+}
